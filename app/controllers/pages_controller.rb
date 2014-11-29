d65dfb197d9c8056170c70b11e8b40e51c9009cd
Dmidd87 <dmidd87@gmail.com>
2014-11-28 20:17:37 -0700

    Added array of cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
 
   def index
+
+    deck = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+    suits = ["S","C","H","D"]
+
+    @deck = deck
+    @suits = [suits]
+
   end
 
 end
