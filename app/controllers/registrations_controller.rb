28ff8917446d8c031aa6bdfb20dc746d2ddd9c58
Dmidd87 <dmidd87@gmail.com>
2015-01-08 10:28:53 -0700

    made registrations controller and added user and create

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < PublicController
+
+  def index
+
+  end
+
+  def user
+    @user = User.new
+  end
+
+  def create
+    @ user = User.new(params.require(:user).permit(
+    :email_address, :first_name, :last_name, :password, :password_confirmation
+    ))
+  end
+end
