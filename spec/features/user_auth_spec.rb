a497087ed593dcefbddc84fec5aec18a15f12d65
Dmidd87 <dmidd87@gmail.com>
2015-01-08 09:39:47 -0700

    Filled in details of tests for registration and user auth

--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -2,28 +2,90 @@ require 'rails_helper'
 
 feature "Users" do
 
+  scenario "There are sign up and sign in links on the home page" do
+    pending
+    visit root_path
+    expect(page).to have_content "Sign In"
+    expect(page).to have_content "Sign Up"
+  end
+
   scenario "A visitor can register as a new user" do
     pending
     visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Bob"
+    fill_in "Last name", with: "Example"
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Register"
+    expect(page).to have_content "Bob Example"
   end
 
   scenario "User cannot sign up without a password" do
     pending
     visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Bob"
+    fill_in "Last name", with: "Example"
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_on "Register"
+    expect(page).to have_content "Password cannot be blank"
   end
 
   scenario "Registered users can sign in" do
     pending
     visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Bob"
+    fill_in "Last name", with: "Example"
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Register"
+    expect(page).to have_content "Bob Example"
+    click_on "Sign Out"
+    click_on "Sign In"
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "password"
   end
 
   scenario "A registered user can sign out" do
     pending
     visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Bob"
+    fill_in "Last name", with: "Example"
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Register"
+    expect(page).to have_content "Bob Example"
+    click_on "Sign Out"
+    expect(page).to have_content "Sign In"
   end
 
-  scenario "A user tries to sign in with a registered email address" do
+  scenario "A user tries to sign up with a registered email address" do
     pending
     visit root_path
+    click_on "Sign Up"
+    fill_in "First name", with: "Bob"
+    fill_in "Last name", with: "Example"
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Register"
+    expect(page).to have_content "Bob Example"
+    click_on "Sign Out"
+    click on "Sign Up"
+    fill_in "First name", with: "New"
+    fill_in "Last name", with: "Test"
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "pass"
+    fill_in "Password confirmation", with: "pass"
+    click_on "Register"
+    expect(page).to have_content "That email address is already taken"
   end
 end
