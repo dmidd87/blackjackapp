dda1c9006d1f60bee0e40b4a9ef0d108be975625
Dmidd87 <dmidd87@gmail.com>
2015-01-08 09:33:17 -0700

    Created some user auth in registrations model and added bcrypt gem

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,14 @@
 class Registration < ActiveRecord::Base
-  
+
+  before_save { |user| user.email_address = email_address.downcase }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  validates :first_name, presence:true
+  validates :last_name, presence:true
+  validates :email_address, presence: true, uniqueness: true
+
+  has_secure_password
 end
