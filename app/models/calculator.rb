d176a4e59ff503afac6c39e94fcb7ddd7a57d538
Dmidd87 <dmidd87@gmail.com>
2015-01-27 13:23:42 -0700

    moved logic to model

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -7,7 +7,7 @@ class Calculator
 
   attr_reader :params, :session
   attr_accessor :game, :dealer_cards, :dealer_cards_value, :player_cards, :player_cards_value, :cards
-  
+
   def initialize(params, session)
     @params = params
     @session = session
@@ -23,52 +23,52 @@ class Calculator
 
     def self.hit
       if params[:commit] == "Hit"
-        self.Card.give_player_a_card(cards)
-        self.cards = Card.where(game_id: game.id)
-        self.player_cards = cards.select{|card| card.player == 'you'}
-        self.player_cards_value = Card.get_value_of_cards(player_cards)
-        self.dealer_cards = cards.select{|card| card.player == 'dealer'}
-        self.dealer_cards_value = Card.get_value_of_cards(dealer_cards)
-        if self.player_cards_value > 21
-          self.game.winner = 'dealer'
-          self.game.save
+        Card.give_player_a_card(cards)
+        cards = Card.where(game_id: game.id)
+        player_cards = cards.select{|card| card.player == 'you'}
+        player_cards_value = Card.get_value_of_cards(player_cards)
+        dealer_cards = cards.select{|card| card.player == 'dealer'}
+        dealer_cards_value = Card.get_value_of_cards(dealer_cards)
+        if player_cards_value > 21
+          game.winner = 'dealer'
+          game.save
         end
       end
     end
 
-    self.dealer_cards = cards.select{|card| card.player == 'dealer'}
-    self.dealer_cards_value = Card.get_value_of_cards(dealer_cards)
-    self.player_cards = cards.select{|card| card.player == 'you' }
-    self.player_cards_value = Card.get_value_of_cards(player_cards)
+    dealer_cards = cards.select{|card| card.player == 'dealer'}
+    dealer_cards_value = Card.get_value_of_cards(dealer_cards)
+    player_cards = cards.select{|card| card.player == 'you' }
+    player_cards_value = Card.get_value_of_cards(player_cards)
 
     def self.stand
       if params[:commit] == "Stand"
-        self.cards.select { |card| card.face_up == false }[0].try(:update, face_up: true)
-        self.dealer_cards_value = self.Card.get_value_of_cards(dealer_cards)
-        if self.dealer_cards_value > self.player_cards_value && self.player_cards_value < 21
-          self.game.winner = 'dealer'
-          self.game.save
+        cards.select { |card| card.face_up == false }[0].try(:update, face_up: true)
+        dealer_cards_value = Card.get_value_of_cards(dealer_cards)
+        if dealer_cards_value > player_cards_value && player_cards_value < 21
+          game.winner = 'dealer'
+          game.save
         end
-        if self.player_cards_value > self.dealer_cards_value && self.player_cards_value < 21
-          self.Card.run_dealers_hand(self.game, self.cards, self.dealer_cards_value, self.player_cards_value)
+        if player_cards_value > dealer_cards_value && player_cards_value < 21
+          Card.run_dealers_hand(game, cards, dealer_cards_value, player_cards_value)
         end
       end
     end
 
     def self.doubledown
       if params[:commit] == "Double Down"
-        self.Card.give_player_a_card(cards)
-        self.cards.select { |card| card.face_up == false }[0].try(:update, face_up: true)
-        self.dealer_cards_value = self.Card.get_value_of_cards(dealer_cards)
-        self.player_cards_value = self.Card.get_value_of_cards(player_cards)
+        Card.give_player_a_card(cards)
+        cards.select { |card| card.face_up == false }[0].try(:update, face_up: true)
+        dealer_cards_value = Card.get_value_of_cards(dealer_cards)
+        player_cards_value = Card.get_value_of_cards(player_cards)
 
-        if self.player_cards_value <= 21 && self.dealer_cards_value < self.player_cards_value
-          self.Card.run_dealers_hand(game, cards, dealer_cards_value, player_cards_value)
+        if player_cards_value <= 21 && dealer_cards_value < player_cards_value
+          Card.run_dealers_hand(game, cards, dealer_cards_value, player_cards_value)
         end
 
         if player_cards_value > 21
-          self.game.winner = 'dealer'
-          self.game.save
+          game.winner = 'dealer'
+          game.save
         end
       end
     end
@@ -78,8 +78,8 @@ class Calculator
     # end
 
     if dealer_cards_value > player_cards_value && dealer_cards_value < 21
-      self.game.winner = 'dealer'
-      self.game.save
+      game.winner = 'dealer'
+      game.save
     end
 
     if dealer_cards_value == 21 && player_cards_value < 21
--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -2,28 +2,78 @@ require 'rails_helper'
 
 describe "Stand" do
   it 'validates that the stand button doesnt give any cards' do
+    pending
     user = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
     user.valid?
     game = Game.create!(user_id: user.id)
     game.valid?
+
     Card.create!(game_id: game.id, player: "you", points: 8)
     Card.create!(game_id: game.id, player: "you", points: 11)
-    Card.create!(game_id: game.id, player: "dealer", points: 10)
-    Card.create!(game_id: game.id, player: "dealer", points: 7, face_up: false)
+
+    Card.create!(game_id: game.id, player: "dealer", points: 6)
+    Card.create!(game_id: game.id, player: "dealer", points: 11, face_up: false)
+
+    [:commit] == "Stand"
+
+    expect(game.reload.winner).to eq("you")
   end
 
 
   it 'validates when it runs the dealers hand that if the dealer busts the player wins' do
+    pending
+    user = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
+    user.valid?
+    game = Game.create!(user_id: user.id)
+    game.valid?
 
+    Card.create!(game_id: game.id, player: "you", points: 8)
+    Card.create!(game_id: game.id, player: "you", points: 11)
+
+    Card.create!(game_id: game.id, player: "dealer", points: 4)
+    Card.create!(game_id: game.id, player: "dealer", points: 10, face_up: false)
+
+    [:commit] == "Stand"
+
+    Card.create!(game_id: game.id, player: "dealer", points: 8)
+
+    expect(game.reload.winner).to eq("you")
   end
 
   it 'validates that when the dealer has less than 21 but a greater value than the player
   the dealer wins' do
+  pending
+  user = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
+  user.valid?
+  game = Game.create!(user_id: user.id)
+  game.valid?
 
+  Card.create!(game_id: game.id, player: "you", points: 8)
+  Card.create!(game_id: game.id, player: "you", points: 11)
+
+  Card.create!(game_id: game.id, player: "dealer", points: 10)
+  Card.create!(game_id: game.id, player: "dealer", points: 10, face_up: false)
+
+  [:commit] == "Stand"
+
+  expect(game.reload.winner).to eq("dealer")
   end
 
   it 'validates that the user cannot click any other buttons after standing' do
 
+    user = User.create!(first_name: "David", last_name: "Example", email_address: "david@example.com", password: "password")
+    user.valid?
+    game = Game.create!(user_id: user.id)
+    game.valid?
+
+    Card.create!(game_id: game.id, player: "you", points: 8)
+    Card.create!(game_id: game.id, player: "you", points: 11)
+
+    Card.create!(game_id: game.id, player: "dealer", points: 9)
+    Card.create!(game_id: game.id, player: "dealer", points: 10, face_up: false)
+
+    [:commit] == "Stand"
+    [:commit] == "Boogers"
   end
 end
 
